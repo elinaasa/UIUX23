3a6e72aee3d0f21e20a66866975a05370a6d6f02
Elina Sankila <sankilaelina@gmail.com>
2023-11-10 11:16:50 +0200

    parallax

---
+++ Parallax/ParallaxHarj/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll', function (event) {
+  var top = this.pageYOffset;
+  console.log(top);
+
+  var layers = document.querySelectorAll('[data-type="parallax"]');
+  var layer, speed, yPos;
+  for (var i = 0; i < layers.length; i++) {
+    layer = layers[i];
+    speed = layer.getAttribute('data-speed');
+    var yPos = -((top * speed) / 100);
+    layer.setAttribute(
+      'style',
+      //   tranlateY(yPos + 'px')
+      'transform: translate3d(0px, ' + yPos + 'px, 0px)'
+    );
+  }
+});
